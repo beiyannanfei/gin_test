07b5947d88f18e20801b51a08f1cac8c1fc9b082
wyq <18810776836@163.com>
2018-03-24 18:31:19 +0800

    update

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"fmt"
 )
 
 func main() {
@@ -12,5 +13,13 @@ func main() {
 	router.GET("/", func(c *gin.Context) {
 		c.String(http.StatusOK, "Hello Gin\n")
 	})
+
+	//curl 127.0.0.1:8080/user/abcd
+	router.GET("/user/:name", func(c *gin.Context) {
+		name := c.Param("name")
+		fmt.Printf("params name: %v\n", name)
+		c.String(http.StatusOK, "Hello %s\n", name)
+	})
+
 	router.Run(":8080")
 }
